1221f918283f0a21735b8c4c8d33e9d1bdfe4c11
Shamil <qurbonovshamil@gmail.com>
2020-03-25 18:37:48 +0700

    Добавлено базовый шаблон nuxt-js

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <h1>Education portal</h1>
+    <el-button type="primary" plain>Adib</el-button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  components: {
+
+  }
+}
+</script>
+
+<style lang="scss">
+.container {
+  text-align: center;
+  margin-top: 200px;
+}
+</style>
