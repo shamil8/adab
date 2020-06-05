82facdc16be57414be5ad83487d74c8e9b563638
Shamil Kurbonov <qurbonovshamil@gmail.com>
2020-06-05 23:11:11 +0700

    Fixed bugs register

--- pages/register.vue
+++ pages/register.vue
@@ -1,23 +1,28 @@
 <template>
   <el-form :model="newUser" status-icon :rules="rules" ref="newUser" class="form-container">
-    <el-form-item prop="name" :error="getErrorForField('name', errors)">
-      <el-input placeholder="Name" type="text" v-model="newUser.name" />
+    <el-form-item
+      prop="name"
+      :error="getErrorForField('name', errors)"
+      :show-message="true" :validate-status="'error'"
+    >
+      <el-input placeholder="Имя и фамилия" :disabled="formProcessing" type="text" v-model="newUser.name" />
     </el-form-item>
 
     <el-form-item prop="email" :error="getErrorForField('email', errors)">
-      <el-input placeholder="Email" type="email" v-model="newUser.email" />
+      <el-input placeholder="E-mail" :disabled="formProcessing" type="email" v-model="newUser.email" />
     </el-form-item>
 
     <el-form-item prop="password" :error="getErrorForField('password', errors)">
-      <el-input placeholder="Password" type="password" v-model="newUser.password"/>
+      <el-input placeholder="Пароль" :disabled="formProcessing" type="password" v-model="newUser.password"/>
     </el-form-item>
 
-    <el-alert
-      v-if="error"
-      :title="error"
-      type="error"
-    />
-    <el-button type="primary" @click="submitForm('newUser')">Sign up</el-button>
+    <el-button
+      :loading="formProcessing"
+      type="primary"
+      @click="submitForm('newUser')"
+    >
+      Sign up
+    </el-button>
 
     <div class="form-group">
       You have an account?
@@ -27,15 +32,15 @@
 </template>
 
 <script>
-  import axios from 'axios';
+  import axios from 'axios'
 
   export default {
     name: "register",
     layout: 'auth',
     data() {
       return {
-        error: '',
         errors: [],
+        formProcessing: false,
         newUser: {
           name: '',
           email: '',
@@ -62,53 +67,71 @@
             { min: 6, message: 'Пароль слишком короткий' },
           ],
         }
-      };
+      }
     },
     methods: {
       getErrorForField(field, errors) {
         if (!errors && !errors.length) {
           return false
         }
-        let filtered = errors.filter(error => {
-          return error.path[0] === field
-        })
+
+        let filtered = errors.filter(error => error.path[0] === field)
+
         if (filtered.length) {
           return filtered[0].message
         }
       },
       submitForm(formName) {
         this.$refs[formName].validate((valid) => {
-          if (valid) {
-            this.newUser.username = this.newUser.email.split('@')[0]
+          if (!valid) {
+            return false
+          }
 
-            axios.post('https://adabapi.ga/api/users', this.newUser)
-              .then(res => {
-                console.log(res.data)
-                this.newUser = {}
+          this.formProcessing = true
+          this.newUser.username = this.newUser.email.split('@')[0]
+
+          this.errors.forEach((error, i, errors) => errors[i].message = '')   // called new message
+
+          axios.post('https://adabapi.ga/api/users', this.newUser, {
+            headers: {
+              'Access-Control-Allow-Origin': '*',
+              'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+            },
+          })
+            .then(res => {
+              console.log(res.data)
+
+              this.newUser = {}
+              this.$message({   // alert message
+                showClose: true,
+                message: 'Авторизация пройдено успешно:)',
+                type: 'success'
               })
-              .catch(err => {
+
+              // this.$store.dispatch('login')
+              // this.$router.push('/')
+            })
+            .catch(err => {
               let data =  err.response.data
 
               if (data.violations) {
-                data.violations.forEach( item =>
-                  this.errors.push({
-                    'path': [item.propertyPath],
-                    'message': item.message
-                  })
-                )
+                data.violations.forEach( (item, i, items) => {
+                  items[i].path = [item.propertyPath]
 
-              } else if(data.error) {
-                this.error = data.error;
-              }else {
-                // this.error = 'Unknown error';
-                this.error = 'Неизвестная ошибка';
+                  delete items[i].propertyPath
+                })
+
+                this.errors = data.violations
+
+              } else {
+                this.$message({   // alert message
+                  showClose: true,
+                  message: data.error ? data.error : 'Неизвестная ошибка',
+                  type: 'error'
+                })
               }
             })
-
-            // this.$store.dispatch('login')
-            // this.$router.push('/')
-          }
-            else return false
+            .then(() => this.formProcessing = false)
         })
       }
     }
